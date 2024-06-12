17fa4ac34266492f106671db5b13001e44817788
Vanshika verma <vanshikav4567@gmail.com>
2024-06-12 19:16:15 +0530

    all files update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async (req, res) => {
     res.status(200).json({
-        message: "ok"
+        message: "hlo i am chai aur code"
     })
 })
 
