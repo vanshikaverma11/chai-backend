41f8157aac69a0bb3d2503378e338970f0362e4a
Vanshika verma <vanshikav4567@gmail.com>
2024-06-13 22:19:33 +0530

    all files update

--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
+import { upload } from "../middlewares/multer.middleware.js"
 
 const router = Router()
 
-router.route("/register").post
+router.route("./register").post
 (
     upload.fields([
         {
