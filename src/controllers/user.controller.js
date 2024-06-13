da240ee70efcef9eaef176d886237629d4c8df09
Vanshika verma <vanshikav4567@gmail.com>
2024-06-13 19:26:28 +0530

    all files updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -24,17 +24,17 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
     const existedUser = await User.findOne({
-        $or: [{ username },{ email }]
+        $or: [{ username }, { email }]
     })
 
     if (existedUser) {
-        throw new ApiError(409, "User name with username and email es already exists")
+        throw new ApiError(409, "User with username and email es already exists")
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
     const coverImageLocalPath = req.files?.coverImage[0]?.path;
     if (!avatarLocalPath) {
-        throw new ApiError(400, "Avtar files is required")
+        throw new ApiError(400, "Avatar files is required")
     }
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
@@ -50,14 +50,14 @@ const registerUser = asyncHandler( async (req, res) => {
         username: username.tolowerCase()
     })
 
-    const craetedUser = await User.findById(user._id).select(
+    const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
-    if (!craetedUser) {
+    if (!createdUser) {
         throw new ApiError(500, "something went wrong while registrating ther user")
     }
     return res.status(201).json(
-        new ApiResponse(200, craetedUser, "user registered successfully")
+        new ApiResponse(200, createdUser, "user registered successfully")
     )
 })
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,74 +1,75 @@
 import mongoose, {Schema} from "mongoose";
-// import { type } from "os";
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim:true,
-        index: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim:true,
-    },
-    fullName: {
-        type: String,
-        required: true,
-        trim:true,
-        index: true
-    },
-    avatar: {
-        type: String,
-        required: true,
-    },
-    coverImage: {
-        type: String,
-    },
-    watchHistory:[
-        {
-            type: Schema.Types.ObjectId,
-            ref: "vidoe"
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true, 
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true, 
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true, 
+            index: true
+        },
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String, // cloudinary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
         }
-    ],
-    password: {
-        type: String,
-        required: [true,'Password is required']
-    },
-    refreshToken: {
-        type: String,
 
+    },
+    {
+        timestamps: true
     }
-},
-{
-    timestamps: true
-}
 )
 
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
+
     this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
-userSchema.methods.ispPsswordCorrect = async function(password){
+userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
 }
+
 userSchema.methods.generateAccessToken = function(){
-    jwt.sign(
+    return jwt.sign(
         {
             _id: this._id,
             email: this.email,
             username: this.username,
             fullName: this.fullName
-            
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
@@ -77,11 +78,9 @@ userSchema.methods.generateAccessToken = function(){
     )
 }
 userSchema.methods.generateRefreshToken = function(){
-    jwt.sign(
+    return jwt.sign(
         {
             _id: this._id,
-            email: this.email
-            
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
@@ -90,4 +89,4 @@ userSchema.methods.generateRefreshToken = function(){
     )
 }
 
-export const User = mongoose.model("User",  userSchema)
+export const User = mongoose.model("User", userSchema)
