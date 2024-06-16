d4714ae9302cb2b11f60a1bf4a217d72513a2e84
Vanshika verma <vanshikav4567@gmail.com>
2024-06-16 14:25:50 +0530

    all files updated

--- src/app.js
+++ src/app.js
@@ -20,8 +20,8 @@ app.use(cookieParser())
 import userRouter from './routes/user.routes.js'
 
 //routes declaraition
-app.use("/api/v1/users", userRouter)
+app.use("/api/v2/users", userRouter)
 
-// http://localhost:8000/api/v1/users/register
+// http://localhost:8000/api/v2/users/register
 
 export { app }
