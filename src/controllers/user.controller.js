f2f202afe9dfe902fa6f8dc25ce0c15fa27fb862
Vanshika verma <vanshikav4567@gmail.com>
2024-06-19 15:07:51 +0530

    all files updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -29,6 +29,9 @@ const registerUser = asyncHandler( async (req, res) => {
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists")
     }
+    
+    console.log(req.files)
+
     const avatarLocalPath = req.files?.avatar[0]?.path;
     const coverimageLocalPath = req.files?.coverimage[0]?.path;
 
