65198d4bebc80bd9b7b2239194d89f38720379b6
Vanshika verma <vanshikav4567@gmail.com>
2024-07-03 20:12:10 +0530

    upload file

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,9 @@ import {ApiError} from "../utils/ApiError.js"
 import { User} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
+
 
 const generateAccessAndRefereshTokens = async(userId) =>{
     try {
@@ -32,56 +35,65 @@ const registerUser = asyncHandler( async (req, res) => {
     // check for user creation
     // return res
 
-    const { fullname, email, username, password } = req.body
+
+    const {fullName, email, username, password } = req.body
     //console.log("email: ", email);
 
-    if ( [fullname, email, username, password].some((field) => 
-    field?.trim() === ""))
-    {
+    if (
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+    ) {
         throw new ApiError(400, "All fields are required")
     }
+
     const existedUser = await User.findOne({
-        $or: [{ username}, {email}]
+        $or: [{ username }, { email }]
     })
+
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists")
     }
-
-    console.log(req.files);
+    //console.log(req.files);
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    //const coverimageLocalPath = req.files?.coverimage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-let coverimageLocalPath;
-if (req.files && Array.isArray(req.files.coverimage) && req.files.coverimage.length > 0) {
-    coverimageLocalPath = req.files.coverimage[0].path 
-}
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+    
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
     }
+
     const avatar = await uploadOnCloudinary(avatarLocalPath)
-    const coverimage = await uploadOnCloudinary(coverimageLocalPath)
-    
-    if(!avatar) {
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!avatar) {
         throw new ApiError(400, "Avatar file is required")
     }
+   
 
     const user = await User.create({
-        fullname,
+        fullName,
         avatar: avatar.url,
-        coverimage: coverimage?.url || "",
-        email,
+        coverImage: coverImage?.url || "",
+        email, 
         password,
         username: username.toLowerCase()
     })
-    const createdUser = await User.findById(user._id).select( "-password -refreshToken" )
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
 
     if (!createdUser) {
         throw new ApiError(500, "Something went wrong while registering the user")
     }
+
     return res.status(201).json(
-        new ApiResponse(200, createdUser, "User register successfully")
+        new ApiResponse(200, createdUser, "User registered Successfully")
     )
 
 } )
@@ -306,8 +318,47 @@ const updateUserAvatar = asyncHandler(async(req, res) => {
         new ApiResponse(200, user, "Avatar image updated successfully")
     )
 })
+
+const updateUserCoverImage = asyncHandler(async(req, res) => {
+    const coverImageLocalPath = req.file?.path
+
+    if (!coverImageLocalPath) {
+        throw new ApiError(400, "Cover image file is missing")
+    }
+
+    //TODO: delete old image - assignment
+
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if (!coverImage.url) {
+        throw new ApiError(400, "Error while uploading on avatar")
+        
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                coverImage: coverImage.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Cover image updated successfully")
+    )
+})
+
 export {
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
 }
